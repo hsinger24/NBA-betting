7e54c40528e4e208e22e034108df05be34e0e294
Henry Singer <hsinger24@Henrys-MBP-2.lan>
2021-08-23 17:49:16 -0500

    Running ML models up to NN

--- machine_learning.R
+++ machine_learning.R
@@ -5,6 +5,7 @@ library(dplyr)
 library(caret)
 library(reshape2)
 library(fastDummies)
+library(deepnet)
 
 ########## Importing data and splitting test and train ##########
 
@@ -104,7 +105,7 @@ gbm = train(training_data,
 predictions_gbm = predict(object = gbm, test_data, type = 'raw')
 prediction_accuracy_gbm= mean(predictions_gbm == test_target) # 66.3
 ##### Stacked AutoEncoder Deep Neural Network #####
-# Formatting data for DNN
+# Formatting data for DNN (Training)
 training_data_dnn = data.frame(training_data)
 training_data_dnn$Team1_Days_Rest_Team = as.double(training_data_dnn$Team1_Days_Rest_Team)
 training_data_dnn$Team1_Days_Rest_Team_Opp = as.double(training_data_dnn$Team1_Days_Rest_Team_Opp)
@@ -116,12 +117,9 @@ training_data_dnn$Team1_Losses = as.double(training_data_dnn$Team1_Losses)
 training_data_dnn$Team1_Losses_Opp = as.double(training_data_dnn$Team1_Losses_Opp)
 training_data_dnn$Team1_USG_PCT_Agg = as.double(training_data_dnn$Team1_USG_PCT_Agg)
 training_data_dnn$Team1_USG_PCT_Agg_Opp = as.double(training_data_dnn$Team1_USG_PCT_Agg_Opp)
-
 training_data_dnn = subset(training_data_dnn, select = -c(86,102))
 training_data_dnn = dummy_cols(training_data_dnn,  remove_most_frequent_dummy = T,
                   remove_selected_columns = T)
-
-
 numerical = c()
 for (i in 1:ncol(training_data_dnn)) {
   if (typeof(training_data_dnn[,i])=='double') {
@@ -130,9 +128,39 @@ for (i in 1:ncol(training_data_dnn)) {
 }
 norm.values = preProcess(training_data_dnn[, numerical], method = 'range')
 training_data_dnn[,numerical] = predict(norm.values, training_data_dnn[, numerical])
-
+# Formatting data for DNN (Test)
+test_data_dnn = data.frame(test_data)
+test_data_dnn$Team1_Days_Rest_Team = as.double(test_data_dnn$Team1_Days_Rest_Team)
+test_data_dnn$Team1_Days_Rest_Team_Opp = as.double(test_data_dnn$Team1_Days_Rest_Team_Opp)
+test_data_dnn$Team1_Days_Next.Game = as.double(test_data_dnn$Team1_Days_Next.Game)
+test_data_dnn$Team1_Days_Next.Game_Opp = as.double(test_data_dnn$Team1_Days_Next.Game_Opp)
+test_data_dnn$Team1_Wins = as.double(test_data_dnn$Team1_Wins)
+test_data_dnn$Team1_Wins_Opp = as.double(test_data_dnn$Team1_Wins_Opp)
+test_data_dnn$Team1_Losses = as.double(test_data_dnn$Team1_Losses)
+test_data_dnn$Team1_Losses_Opp = as.double(test_data_dnn$Team1_Losses_Opp)
+test_data_dnn$Team1_USG_PCT_Agg = as.double(test_data_dnn$Team1_USG_PCT_Agg)
+test_data_dnn$Team1_USG_PCT_Agg_Opp = as.double(test_data_dnn$Team1_USG_PCT_Agg_Opp)
+test_data_dnn = subset(test_data_dnn, select = -c(86,102))
+test_data_dnn = dummy_cols(test_data_dnn,  remove_most_frequent_dummy = T,
+                               remove_selected_columns = T)
+numerical_test = c()
+for (i in 1:ncol(test_data_dnn)) {
+  if (typeof(test_data_dnn[,i])=='double') {
+    numerical_test = append(numerical_test, colnames(test_data_dnn)[i])
+  }
+}
+norm.values = preProcess(test_data_dnn[, numerical_test], method = 'range')
+test_data_dnn[,numerical_test] = predict(norm.values, test_data[, numerical_test])
 # DNN
 dnn = train(training_data_dnn,
                training_target,
                trControl = ctrl,
                method='dnn')
+test_data_final = cbind(test_data_dnn, test_target)
+test_data_matrix = model.matrix(test_target~., data = test_data_final)[,-1]
+predictions_dnn = nn.predict(dnn, test_data_matrix) # Not working (yet)
+
+# Rotation Forest
+# Robust SIMCA
+# Oblique Random Forest
+# Multilayer Perceptron Network with Weight Decay
