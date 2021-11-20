ab4138c66213aed53c64a88da98a32cb101f27e5
Henry Singer <henrysinger24@gmail.com>
2021-11-20 09:12:10 -0700

    Daily run

--- run_daily.py
+++ run_daily.py
@@ -19,10 +19,10 @@ from current_season_code.functions import *
 
 ##########NECESSARY VARIABLES##########
 
-todays_capital = 110052
-yesterdays_capital = 110052
+todays_capital = 113006
+yesterdays_capital = 113006
 year = 2021
-continue_value_advanced = 230
+continue_value_advanced = 239
 continue_value_traditional = 230
 ml_param = -1750
 ml_param_underdog = 1000
@@ -31,15 +31,15 @@ kelly = 12
 
 ##########RUN##########
 
-retrieve_advanced_stats(year = year, continue_value=continue_value_advanced)
-retrieve_traditional_stats(year = year, continue_value=continue_value_traditional)
-formatted_api_data = format_api_data(year = year)
-formatted_data_1(formatted_api_data)
-final_stats = formatted_data_2()
-odds = retrieve_odds(save = True)
-todays_games(odds)
-calculate_bet_results(yesterdays_capital)
+# retrieve_advanced_stats(year = year, continue_value=continue_value_advanced)
+# retrieve_traditional_stats(year = year, continue_value=continue_value_traditional)
+# formatted_api_data = format_api_data(year = year)
+# formatted_data_1(formatted_api_data)
+# final_stats = formatted_data_2()
+# odds = retrieve_odds(save = True)
+# todays_games(odds)
+# calculate_bet_results(yesterdays_capital)
 
 ########## RUN THE R FILE ##########
 
-# calculate_bets(todays_capital = todays_capital, ml_param = ml_param, ml_param_underdog = ml_param_underdog, small_advantage = small_advantage, kelly = kelly)
+calculate_bets(todays_capital = todays_capital, ml_param = ml_param, ml_param_underdog = ml_param_underdog, small_advantage = small_advantage, kelly = kelly)
