4699bd3bb1e4f919ca0448c1c530673c7818b160
Henry Singer <henrysinger24@gmail.com>
2021-12-20 18:12:16 -0600

    Daily run + fixes for PPD games

--- run_daily.py
+++ run_daily.py
@@ -20,8 +20,8 @@ from current_season_code.functions import *
 ########## NECESSARY VARIABLES ##########
 
 year = 2021
-continue_value_advanced = 448
-continue_value_traditional = 448
+continue_value_advanced = 457 # Missed = 452 - 454
+continue_value_traditional = 457 # Missed = 452 - 454
 ml_param = -1750
 ml_param_underdog = 1000
 small_advantage = .025
@@ -29,11 +29,11 @@ kelly = 12
 
 ########## RUN ##########
 
-# results = pd.read_csv('current_season_data/results_tracker.csv')
-# capital = results.loc[len(results)-1, 'Money_Tracker']
-# results_external = pd.read_csv('current_season_data/results_tracker_external.csv')
-# capital_538 = results_external.loc[len(results_external)-1, 'Money_Tracker_538']
-# capital_combined = results_external.loc[len(results_external)-1, 'Money_Tracker_Combined']
+results = pd.read_csv('current_season_data/results_tracker.csv')
+capital = results.loc[len(results)-1, 'Money_Tracker']
+results_external = pd.read_csv('current_season_data/results_tracker_external.csv')
+capital_538 = results_external.loc[len(results_external)-1, 'Money_Tracker_538']
+capital_combined = results_external.loc[len(results_external)-1, 'Money_Tracker_Combined']
 
 # retrieve_advanced_stats(year = year, continue_value=continue_value_advanced)
 # retrieve_traditional_stats(year = year, continue_value=continue_value_traditional)
@@ -42,17 +42,17 @@ kelly = 12
 # final_stats = formatted_data_2()
 # odds = retrieve_odds(save = True)
 # todays_games(odds)
-# calculate_bet_results(capital)
-# calculate_bet_results_external(yesterdays_capital_538 = capital_538, yesterdays_capital_combined = capital_combined)
+calculate_bet_results(capital)
+calculate_bet_results_external(yesterdays_capital_538 = capital_538, yesterdays_capital_combined = capital_combined)
 
 ########## RUN THE R FILE ##########
 
-results = pd.read_csv('current_season_data/results_tracker.csv')
-capital = results.loc[len(results)-1, 'Money_Tracker']
-results_external = pd.read_csv('current_season_data/results_tracker_external.csv')
-capital_538 = results_external.loc[len(results_external)-1, 'Money_Tracker_538']
-capital_combined = results_external.loc[len(results_external)-1, 'Money_Tracker_Combined']
+# results = pd.read_csv('current_season_data/results_tracker.csv')
+# capital = results.loc[len(results)-1, 'Money_Tracker']
+# results_external = pd.read_csv('current_season_data/results_tracker_external.csv')
+# capital_538 = results_external.loc[len(results_external)-1, 'Money_Tracker_538']
+# capital_combined = results_external.loc[len(results_external)-1, 'Money_Tracker_Combined']
 
-calculate_bets(todays_capital = capital, ml_param = ml_param, ml_param_underdog = ml_param_underdog, small_advantage = small_advantage, kelly = kelly)
-calculate_bets_external(todays_capital_538 = capital_538, todays_capital_combined = capital_combined, ml_param = ml_param, ml_param_underdog = ml_param_underdog, small_advantage = small_advantage) 
+# calculate_bets(todays_capital = capital, ml_param = ml_param, ml_param_underdog = ml_param_underdog, small_advantage = small_advantage, kelly = kelly)
+# calculate_bets_external(todays_capital_538 = capital_538, todays_capital_combined = capital_combined, ml_param = ml_param, ml_param_underdog = ml_param_underdog, small_advantage = small_advantage) 
 
